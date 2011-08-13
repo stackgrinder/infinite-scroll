053612f1610c867a65f9300b652b14d99378d597
Beaver6813 <Beaver6813@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-13 16:37:08 +0000

    Initial PresetDB commit.
    git-svn-id: https://plugins.svn.wordpress.org/infinite-scroll/branches/PresetDB@422953 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ PresetDB.csv.php
@@ -0,0 +1,4 @@
+<?php /*Infinite-Scroll Preset DB. To use this in other programs, remove this and the bottom line.
+Version,110813
+inove,#main,"#main div.post",#pagenavi,"#pagenavi span.older a:first"
+End Preset DB*/ ?>
